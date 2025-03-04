766078b2ec41f43f03d1fe55e33d8fc9f17ae884
irishgreencitrus <irishgreencitrus@gmail.com>
2025-03-04 18:37:44 +0000

    Fix accents not being properly loaded into lang files.

--- src/main/java/io/github/irishgreencitrus/occultengineering/datagen/DataProviders.java
+++ src/main/java/io/github/irishgreencitrus/occultengineering/datagen/DataProviders.java
@@ -1,9 +1,18 @@
 package io.github.irishgreencitrus.occultengineering.datagen;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.simibubi.create.foundation.utility.FilesHelper;
+import com.tterrag.registrate.providers.ProviderType;
+import io.github.irishgreencitrus.occultengineering.OccultEngineering;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataProviders {
@@ -11,5 +20,26 @@ public class DataProviders {
     public static void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
         generator.addProvider(event.includeServer(), new PentacleProvider(generator));
+
+        OccultEngineering.REGISTRATE.addDataGenerator(ProviderType.LANG, provider -> {
+            BiConsumer<String, String> langConsumer = provider::add;
+
+            provideDefaultLang("interface", langConsumer);
+            provideDefaultLang("tooltips", langConsumer);
+        });
+    }
+
+    private static void provideDefaultLang(String fileName, BiConsumer<String, String> consumer) {
+        String path = "assets/occultengineering/lang/default/" + fileName + ".json";
+        JsonElement jsonElement = FilesHelper.loadJsonResource(path);
+        if (jsonElement == null) {
+            throw new IllegalStateException(String.format("Could not find default lang file: %s", path));
+        }
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+        for (Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+            String key = entry.getKey();
+            String value = new String(entry.getValue().getAsString().getBytes(), StandardCharsets.UTF_8);
+            consumer.accept(key, value);
+        }
     }
 }
