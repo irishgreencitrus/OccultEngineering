eb5572fb8a3bb7294e35158ea5be1ff4c18365f7
irishgreencitrus <irishgreencitrus@gmail.com>
2025-10-16 20:33:40 +0100

    fix: Make the Otherworld Detector work properly on dedicated servers.

--- src/main/java/io/github/irishgreencitrus/occultengineering/content/block/otherworld_detector/OtherworldDetectorBlockEntity.java
+++ src/main/java/io/github/irishgreencitrus/occultengineering/content/block/otherworld_detector/OtherworldDetectorBlockEntity.java
@@ -1,17 +1,20 @@
 package io.github.irishgreencitrus.occultengineering.content.block.otherworld_detector;
 
 import com.klikli_dev.occultism.Occultism;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
 import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
+import io.github.irishgreencitrus.occultengineering.content.block.otherworld_detector.packet.ThirdEyeActivationPacket;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.List;
 
 public class OtherworldDetectorBlockEntity extends SmartBlockEntity {
-    // TODO: provide a codec for this block entity so it can be properly networked.
     public OtherworldDetectorBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState blockState) {
         super(type, pos, blockState);
         setLazyTickRate(20);
@@ -23,41 +26,43 @@ public class OtherworldDetectorBlockEntity extends SmartBlockEntity {
     @Override
     public void tick() {
         super.tick();
-        if (level == null) return;
-        if (level.isClientSide) return;
+        if (level == null || level.isClientSide) return;
+
         BlockState state = getBlockState();
         if (turnOffInTicks > 0) {
             turnOffInTicks--;
             if (turnOffInTicks == 0) {
-                level.scheduleTick(worldPosition, state.getBlock(), 1);
+                level.setBlockAndUpdate(worldPosition, state.setValue(OtherworldDetectorBlock.POWERED, false));
+                level.updateNeighborsAt(worldPosition, state.getBlock());
             }
         }
+        var centre = worldPosition.getCenter();
 
-        var nearestPlayer = level.getNearestPlayer(worldPosition.getX(), worldPosition.getY(), worldPosition.getZ(), 15.0, pl -> !pl.isSpectator());
-
+        var nearestPlayer = level.getNearestPlayer(centre.x, centre.y, centre.z, 15.0, pl -> !pl.isSpectator());
         if (nearestPlayer == null) return;
-        if (Occultism.THIRD_EYE_EFFECT_RENDERER.gogglesActiveLastTick || Occultism.THIRD_EYE_EFFECT_RENDERER.thirdEyeActiveLastTick) {
-            var distanceSqr = nearestPlayer.distanceToSqr(worldPosition.getX(), worldPosition.getY(), worldPosition.getZ());
+
+        if (ThirdEyeActivationPacket.isActive(nearestPlayer)) {
+            var distanceSqr = nearestPlayer.distanceToSqr(centre.x, centre.y, centre.z);
             var distance = Math.sqrt(distanceSqr);
             // Stronger the closer you are
-            comparatorSignalStrength = 15 - (int) distance;
-
-            activate(4);
+            var newSignal = Math.max(0,15 - (int) distance);
+            activate(newSignal,4);
         }
     }
 
-    public void activate(int ticks) {
+    public void activate(int signalStrength, int ticks) {
         if (level == null) return;
 
         BlockState state = getBlockState();
         turnOffInTicks = ticks;
-        //if (state.getValue(OtherworldDetectorBlock.POWERED)) return;
-        level.setBlockAndUpdate(worldPosition, state.setValue(OtherworldDetectorBlock.POWERED, true));
-        level.updateNeighborsAt(worldPosition, state.getBlock());
+        if (signalStrength != comparatorSignalStrength) {
+            comparatorSignalStrength = signalStrength;
+            level.setBlockAndUpdate(worldPosition, state.setValue(OtherworldDetectorBlock.POWERED, true));
+            level.updateNeighborsAt(worldPosition, state.getBlock());
+        }
     }
 
     @Override
     public void addBehaviours(List<BlockEntityBehaviour> list) {
     }
-
 }
