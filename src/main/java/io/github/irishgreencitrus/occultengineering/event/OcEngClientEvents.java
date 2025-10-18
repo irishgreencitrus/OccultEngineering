0d34ed42e4ecec054f1ccd1308562f69895808f8
irishgreencitrus <irishgreencitrus@gmail.com>
2025-10-18 14:02:32 +0100

    fix: Update the THIRD_EYE data tag when a player joins the world.

--- src/main/java/io/github/irishgreencitrus/occultengineering/event/OcEngClientEvents.java
+++ src/main/java/io/github/irishgreencitrus/occultengineering/event/OcEngClientEvents.java
@@ -1,7 +1,6 @@
 package io.github.irishgreencitrus.occultengineering.event;
 
 import com.klikli_dev.occultism.Occultism;
-import io.github.irishgreencitrus.occultengineering.OccultEngineering;
 import io.github.irishgreencitrus.occultengineering.content.block.otherworld_detector.packet.ThirdEyeActivationPacket;
 import net.createmod.catnip.platform.CatnipServices;
 import net.minecraft.client.Minecraft;
@@ -10,10 +9,25 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.event.level.LevelEvent;
 
 @EventBusSubscriber(Dist.CLIENT)
 public class OcEngClientEvents {
     private static boolean lastState = false;
+
+    @SubscribeEvent
+    public static void onClientWorldLoad(LevelEvent.Load event) {
+        if (event.getLevel().isClientSide()) {
+            Minecraft.getInstance().tell(() -> {
+                        boolean currentState = Occultism.THIRD_EYE_EFFECT_RENDERER.gogglesActiveLastTick
+                                || Occultism.THIRD_EYE_EFFECT_RENDERER.thirdEyeActiveLastTick;
+                        lastState = currentState;
+                        CatnipServices.NETWORK.sendToServer(new ThirdEyeActivationPacket(currentState));
+                    }
+            );
+        }
+    }
+
     @SubscribeEvent
     public static void onClientTick(ClientTickEvent.Post event) {
         Player player = Minecraft.getInstance().player;
