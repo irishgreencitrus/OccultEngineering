8db5b5da6ed8d38c3813d68dce597fc6e45b0e05
irishgreencitrus <irishgreencitrus@gmail.com>
2026-01-10 15:27:13 +0000

    feat: Add all chalks to Schematic Placement

--- src/main/java/io/github/irishgreencitrus/occultengineering/registry/OccultEngineeringSchematic.java
+++ src/main/java/io/github/irishgreencitrus/occultengineering/registry/OccultEngineeringSchematic.java
@@ -1,35 +1,52 @@
 package io.github.irishgreencitrus.occultengineering.registry;
 
+import com.klikli_dev.occultism.common.block.ChalkGlyphBlock;
 import com.klikli_dev.occultism.registry.OccultismBlocks;
-import com.klikli_dev.occultism.registry.OccultismItems;
 import com.simibubi.create.api.schematic.requirement.SchematicRequirementRegistries;
 import com.simibubi.create.content.schematics.requirement.ItemRequirement;
 import com.tterrag.registrate.util.entry.BlockEntry;
-import com.tterrag.registrate.util.entry.ItemEntry;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredItem;
+
+import java.util.List;
 
 public class OccultEngineeringSchematic {
     public static void register() {
-        registerDamageItem(OccultEngineeringBlocks.COPPER_CHALK, OccultEngineeringItems.COPPER_CHALK);
-        registerDamageItem(OccultEngineeringBlocks.ZINC_CHALK, OccultEngineeringItems.ZINC_CHALK);
-        registerDamageItem(OccultEngineeringBlocks.BRASS_CHALK, OccultEngineeringItems.BRASS_CHALK);
-        registerDamageItem(OccultismBlocks.CHALK_GLYPH_WHITE, OccultismItems.CHALK_WHITE);
-        registerDamageItem(OccultismBlocks.CHALK_GLYPH_YELLOW, OccultismItems.CHALK_YELLOW);
-        registerDamageItem(OccultismBlocks.CHALK_GLYPH_RED, OccultismItems.CHALK_RED);
-        registerDamageItem(OccultismBlocks.CHALK_GLYPH_PURPLE, OccultismItems.CHALK_PURPLE);
+        registerDamageChalk(OccultEngineeringBlocks.COPPER_CHALK);
+        registerDamageChalk(OccultEngineeringBlocks.ZINC_CHALK);
+        registerDamageChalk(OccultEngineeringBlocks.BRASS_CHALK);
+        var occultismChalks = List.of(
+                OccultismBlocks.CHALK_GLYPH_WHITE,
+                OccultismBlocks.CHALK_GLYPH_YELLOW,
+                OccultismBlocks.CHALK_GLYPH_PURPLE,
+                OccultismBlocks.CHALK_GLYPH_RED,
+                OccultismBlocks.CHALK_GLYPH_LIGHT_GRAY,
+                OccultismBlocks.CHALK_GLYPH_GRAY,
+                OccultismBlocks.CHALK_GLYPH_BLACK,
+                OccultismBlocks.CHALK_GLYPH_BROWN,
+                OccultismBlocks.CHALK_GLYPH_ORANGE,
+                OccultismBlocks.CHALK_GLYPH_LIME,
+                OccultismBlocks.CHALK_GLYPH_GREEN,
+                OccultismBlocks.CHALK_GLYPH_CYAN,
+                OccultismBlocks.CHALK_GLYPH_LIGHT_BLUE,
+                OccultismBlocks.CHALK_GLYPH_BLUE,
+                OccultismBlocks.CHALK_GLYPH_MAGENTA,
+                OccultismBlocks.CHALK_GLYPH_PINK,
+                OccultismBlocks.CHALK_GLYPH_RAINBOW,
+                OccultismBlocks.CHALK_GLYPH_VOID
+        );
+
+        for (var c : occultismChalks) registerDamageChalk(c);
     }
 
-    private static void registerDamageItem(DeferredBlock<?> block, DeferredItem<?> item) {
-        registerDamageItem(block.get(), item.get());
+    private static void registerDamageChalk(BlockEntry<ChalkGlyphBlock> glyph) {
+        registerDamageItem(glyph.get(), glyph.get().getChalk());
     }
 
-    private static void registerDamageItem(BlockEntry<?> block, ItemEntry<?> item) {
-        registerDamageItem(block.get(), item.get());
+    private static void registerDamageChalk(DeferredBlock<? extends ChalkGlyphBlock> glyph) {
+        registerDamageItem(glyph.get(), glyph.get().getChalk());
     }
-
 
     private static void registerDamageItem(Block block, Item item) {
         SchematicRequirementRegistries.BLOCKS.register(block, (state, blockEntity) -> new ItemRequirement(ItemRequirement.ItemUseType.DAMAGE, item));
